13cc4c2c8f5f0483c9229c0e2c08459452c8e3c1
jmuller4 <jmuller4@IST-IE103-007.ccsd.d5>
2015-01-21 17:17:11 -0700

    Got PIXI.Stage/ Scene Managing, so I will be able to switch scenes and deal with them separately Not really familiar with switching Scenes, this first tutorial seems really good, like it'll help.
    Oh, and even though the tutorial is in TypeScript, I can write custom Scenes because have tools to do that:
    (Visual Studio, or Microsoft's online TypeScript converter)

---
+++ VideoGame2015/public_html/js/ScenesManager.js
@@ -0,0 +1,64 @@
+// following this tutorial: http://ezelia.com/2013/pixi-tutorial
+// originally written in TypeScript, translated and converted to fit my situation
+
+// actually, now that tutorial is imported
+// and can convert from that TypeScript to JavaScript
+// no need for this file anymore
+
+ScenesManager = {
+    scenes: {}, // should be hashmap but a JS object is fine too :)
+    currentScene: null,   // Scene
+    renderer: null, //PIXI.IRenderer;
+    
+    create: function(width, height) {
+        if (this.renderer) return this;
+        
+        this.renderer = PIXI.autoDetectRenderer(width, height);
+        document.body.appendChild(this.renderer.view);
+        requestAnimFrame(this.loop);
+        return this;
+    },
+    
+    
+    loop: function() {
+        // do the looping
+        requestAnimFrame(function () { this.loop() });
+        
+        
+        // does not exist
+        // won't have paused scenes in game (for now anyway)
+        // also, not implementing paused because not gonna 'extend' Stage
+        if (!this.currentScene) return;
+        
+        
+        currentScene.update();
+        this.renderer.render(this.currentScene);
+    },
+    
+    
+    createScene: function(id, givenScene) {
+        if (this.scenes[id]) return undefined;
+        
+        var scene;
+        if (givenScene) {
+            scene = givenScene;
+        }
+        else {
+            scene = new Scene();
+        }
+        this.scenes[id] = scene;
+        
+        return scene;
+    },
+    
+    goToScene: function(id) {
+        
+        if (this.scenes[id]) {
+            if (this.currentScene) this.currentScene.pause();
+            this.currentScene = this.scenes[id];
+            this.currentScene.resume();
+            return true;
+        }
+        return false;
+    },
+}
